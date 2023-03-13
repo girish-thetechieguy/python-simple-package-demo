e972d5f4d1ec247c91cf91225da3defe17f6136a
girish-thetechieguy <girish.thetechieguy@gmail.com>
2023-03-13 16:21:09 +0800

    Added the first package for complete module runner

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-simple-package-demo",
+    version="1.0.0",
+    author="Girish B",
+    description="Sample package manager",
+
+    packages=find_packages(),
+    classifiers=[
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['beautifulsoup4', 'requests', 
+                      'pandas', 'numpy','timedelta'],
+)
